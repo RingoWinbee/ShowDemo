5b89a2f5ca682135e87f84a6e7ae1f1ec9755349
RingoWinbee <13922924658@163.com>
2019-05-24 00:27:22 +0800

    这个是用来展示怎么用Jenkins的demo

---
+++ ShowDemo/target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.ringo.ShowDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
